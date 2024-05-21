bc1535c0a17bac062f439ab1b5c851045ec3f51d
ArianCSN <arianniazi2002@gmail.com>
2024-05-21 20:55:27 +0330

    Modified Map09.py
    Added a wall spawning mechanism that, when triggered, spawns walls over time to kill the player.

--- Map09.py
+++ Map09.py
@@ -1,7 +1,6 @@
 import pygame
 from Ground import Ground
 from MyPlayer import MyPlayer
-from Bot import Bot
 from Wall import Wall
 from InvisibleWall import InvisibleWall
 
@@ -78,6 +77,51 @@ def map09(x_pos, y_pos, developer_mode):
     # - InvisibleWall objects handle map transitions
     mp = [InvisibleWall(1533, 420, 2, 90), InvisibleWall(0, 240, 2, 90)]
 
+    # Buttons that trigger the wall spawning mechanism when the player collides with them :
+    # - The first button is for when the player moves from the left side of the map to the right.
+    # - The second button is for when the player moves from the right side of the map to the left.
+    triggers = [InvisibleWall(180, 270, 30, 30), InvisibleWall(1350, 450, 30, 30)]
+
+    # Walls defined before appending them to the killer wall list
+    walls_append = [Wall(0, 0, 30, 864, wall_texture), Wall(30, 0, 30, 864, wall_texture),
+                    Wall(60, 0, 30, 864, wall_texture ), Wall(90, 0, 30, 864, wall_texture),
+                    Wall(120, 0, 30, 864, wall_texture), Wall(150, 0, 30, 864, wall_texture),
+                    Wall(180, 0, 30, 864, wall_texture), Wall(210, 0, 30, 864, wall_texture),
+                    Wall(240, 0, 30, 864, wall_texture), Wall(270, 0, 30, 864, wall_texture),
+                    Wall(300, 0, 30, 864, wall_texture), Wall(330, 0, 30, 864, wall_texture),
+                    Wall(360, 0, 30, 864, wall_texture), Wall(390, 0, 30, 864, wall_texture),
+                    Wall(420, 0, 30, 864, wall_texture), Wall(450, 0, 30, 864, wall_texture),
+                    Wall(480, 0, 30, 864, wall_texture), Wall(510, 0, 30, 864, wall_texture),
+                    Wall(540, 0, 30, 864, wall_texture), Wall(570, 0, 30, 864, wall_texture),
+                    Wall(600, 0, 30, 864, wall_texture), Wall(630, 0, 30, 864, wall_texture),
+                    Wall(660, 0, 30, 864, wall_texture), Wall(690, 0, 30, 864, wall_texture),
+                    Wall(720, 0, 30, 864, wall_texture), Wall(750, 0, 30, 864, wall_texture),
+                    Wall(780, 0, 30, 864, wall_texture), Wall(810, 0, 30, 864, wall_texture),
+                    Wall(840, 0, 30, 864, wall_texture), Wall(870, 0, 30, 864, wall_texture),
+                    Wall(900, 0, 30, 864, wall_texture), Wall(930, 0, 30, 864, wall_texture),
+                    Wall(960, 0, 30, 864, wall_texture), Wall(990, 0, 30, 864, wall_texture),
+                    Wall(1020, 0, 30, 864, wall_texture), Wall(1050, 0, 30, 864, wall_texture),
+                    Wall(1080, 0, 30, 864, wall_texture), Wall(1110, 0, 30, 864, wall_texture),
+                    Wall(1140, 0, 30, 864, wall_texture), Wall(1170, 0, 30, 864, wall_texture),
+                    Wall(1200, 0, 30, 864, wall_texture), Wall(1230, 0, 30, 864, wall_texture),
+                    Wall(1260, 0, 30, 864, wall_texture), Wall(1290, 0, 30, 864, wall_texture),
+                    Wall(1320, 0, 30, 864, wall_texture), Wall(1350, 0, 30, 864, wall_texture),
+                    Wall(1380, 0, 30, 864, wall_texture), Wall(1410, 0, 30, 864, wall_texture),
+                    Wall(1440, 0, 30, 864, wall_texture), Wall(1470, 0, 30, 864, wall_texture),
+                    Wall(1500, 0, 30, 864, wall_texture), Wall(1530, 0, 36, 864, wall_texture)]
+
+    # Define an empty list for killer walls that can kill the player
+    killer_walls = []
+
+    # Trigger variable for activating the wall spawning mechanism:
+    trigger = 0
+
+    # Count variable for wall spawning
+    clk_for_walls = 0
+
+    # Speed of wall spawn
+    speed_of_walls = 55
+
     # main loop
     running = True
     while running:
@@ -101,6 +145,56 @@ def map09(x_pos, y_pos, developer_mode):
         for wall in walls:
             wall.draw(screen)
 
+        # Draw the killer walls
+        for wall in killer_walls:
+            wall.draw(screen)
+
+        # Check for player collisions with killer walls
+        for killer_wall in killer_walls:
+            # if player collide killer walls it goes to game over screen and pass player position of map01 for new start
+            if player.rect.colliderect(killer_wall.rect):
+                sound_effect = pygame.mixer.Sound('assets/map09/sound/lose.wav')
+                sound_effect.set_volume(0.2)
+                sound_effect.play()
+                pygame.mixer.music.stop()
+                return "game_over", 780, 390
+
+        # Handle triggers:
+        # - If the player collides with the first trigger, set the trigger variable to 1 and clear the triggers list.
+        # - If the player collides with the second trigger, set the trigger variable to 2 and clear the triggers list.
+        if triggers and player.rect.colliderect(triggers[0]):
+            trigger = 1
+            triggers.clear()
+
+        if triggers and player.rect.colliderect(triggers[1]):
+            trigger = 2
+            triggers.clear()
+
+        # Wall spawning logic:
+        # - When trigger is 1 (player on the left), spawn walls from the left.
+        # - When clk_for_walls % speed_of_walls becomes 0, append the first wall from the walls_append list
+        #   to killer walls and remove it from walls_append.
+        if trigger == 1:
+            clk_for_walls += 1
+            if (clk_for_walls % speed_of_walls) == 0 and walls_append:
+                killer_walls.append(walls_append[0])
+                walls_append.remove(walls_append[0])
+                sound_effect = pygame.mixer.Sound('assets/map09/sound/place.wav')
+                sound_effect.set_volume(0.2)
+                sound_effect.play()
+
+        # - When trigger is 2 (player on the right), spawn walls from the right.
+        # - When clk_for_walls % speed_of_walls becomes 0, append the last wall from the walls_append list
+        #   to killer walls and remove it from walls_append.
+        if trigger == 2:
+            clk_for_walls += 1
+            if (clk_for_walls % speed_of_walls) == 0 and walls_append:
+                killer_walls.append(walls_append[-1])
+                walls_append.remove(walls_append[-1])
+                sound_effect = pygame.mixer.Sound('assets/map09/sound/place.wav')
+                sound_effect.set_volume(0.2)
+                sound_effect.play()
+
         # player collide check with walls
         player.check_collision(walls)
 
@@ -121,6 +215,10 @@ def map09(x_pos, y_pos, developer_mode):
             player.draw_rect(screen)
             for mp_dev in mp:
                 mp_dev.draw(screen, (0, 0, 255))
+
+            for trig in triggers:
+                trig.draw(screen, (0, 0, 255))
+
 
         # Frame rate
         pygame.time.Clock().tick(240)
