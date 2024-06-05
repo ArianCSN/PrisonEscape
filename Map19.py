0e67ccc5383eb6272505ec74b36305a3d6f6c9f8
ArianCSN <arianniazi2002@gmail.com>
2024-06-05 13:32:26 +0330

    Added Map19.py
    Created map19 and linked map changer number 0 a teleport to map15

---
+++ Map19.py
@@ -0,0 +1,175 @@
+import pygame
+from Ground import Ground
+from MyPlayer import MyPlayer
+from Bot import Bot
+from Wall import Wall
+from InvisibleWall import InvisibleWall
+
+
+def map19(x_pos, y_pos, developer_mode):
+    # start the pygame and pygame mixer
+    pygame.init()
+    pygame.mixer.init()
+
+    # set up screen
+    screen_width = 1536
+    screen_height = 864
+    pygame.display.set_caption('Prison Escape')
+    screen = pygame.display.set_mode((screen_width, screen_height), pygame.FULLSCREEN)
+
+    # Hide the mouse cursor
+    if not developer_mode:
+        pygame.mouse.set_visible(False)
+
+    # load texture
+    ground_texture = pygame.image.load('assets/map19/ground/ground.jpg')
+
+    wall_texture = pygame.image.load('assets/map19/wall/wall.png')
+
+    idle = pygame.image.load('assets/map19/player/idle.png')
+
+    walk_up = None
+
+    walk_left = [pygame.image.load('assets/map19/player/L1.png'), pygame.image.load('assets/map19/player/L2.png'),
+                 pygame.image.load('assets/map19/player/L3.png'), pygame.image.load('assets/map19/player/L4.png'),
+                 pygame.image.load('assets/map19/player/L5.png'), pygame.image.load('assets/map19/player/L6.png')]
+
+    walk_down = None
+
+    walk_right = [pygame.image.load('assets/map19/player/R1.png'), pygame.image.load('assets/map19/player/R2.png'),
+                  pygame.image.load('assets/map19/player/R3.png'), pygame.image.load('assets/map19/player/R4.png'),
+                  pygame.image.load('assets/map19/player/R5.png'), pygame.image.load('assets/map19/player/R6.png')]
+
+    bot_idle = pygame.image.load('assets/map19/bot/idle.png')
+
+    bot_up = None
+
+    bot_left = [pygame.image.load('assets/map19/bot/L1.png'), pygame.image.load('assets/map19/bot/L2.png'),
+                pygame.image.load('assets/map19/bot/L3.png'), pygame.image.load('assets/map19/bot/L4.png'),
+                pygame.image.load('assets/map19/bot/L5.png'), pygame.image.load('assets/map19/bot/L6.png')]
+
+    bot_down = None
+
+    bot_right = [pygame.image.load('assets/map19/bot/R1.png'), pygame.image.load('assets/map19/bot/R2.png'),
+                 pygame.image.load('assets/map19/bot/R3.png'), pygame.image.load('assets/map19/bot/R4.png'),
+                 pygame.image.load('assets/map19/bot/R5.png'), pygame.image.load('assets/map19/bot/R6.png')]
+
+    # define the ground
+    ground = Ground(screen_width, screen_height, ground_texture)
+
+    # Create the player character
+    # The player is represented by an instance of the MyPlayer class:
+    # - (x_pos, y_pos): Initial position on the screen
+    # - screen: Pygame screen object
+    # - Textures for different directions (walk_up, walk_left, walk_down, walk_right, idle)
+    # - Speed (5th input): Controls how fast the player moves (higher values mean faster movement)
+    # - developer_mode: A flag indicating whether to enable developer-specific features
+    player = MyPlayer(x_pos, y_pos, screen, walk_up, walk_left, walk_down, walk_right, idle, 5, developer_mode)
+
+    # Define the bots
+    # Each bot is initialized with specific parameters:
+    # - (x, y): Initial position on the screen
+    # - screen: Pygame screen object
+    # - Textures for different directions (up, left, down, right, idle)
+    # - Speed (9th input): Controls how fast the bot moves (higher values mean faster movement)
+    # - developer_mode: A flag indicating whether to enable developer-specific features
+    bots = [Bot(660, 600, screen, bot_up, bot_left, bot_down, bot_right, bot_idle, 2, developer_mode),
+            Bot(1140, 360, screen, bot_up, bot_left, bot_down, bot_right, bot_idle, 2, developer_mode)]
+
+    # Define the walls
+    # Each wall is represented by a rectangular area with specific dimensions:
+    # - (x, y): Top-left corner coordinates
+    # - Width and height of the wall
+    # - Texture (wall_texture) used for rendering
+    walls = [Wall(0, 0, 630, 864, wall_texture), Wall(750, 0, 786, 330, wall_texture),
+             Wall(750, 450, 786, 414, wall_texture)]
+
+    # Hidden walls that change maps
+    # These invisible walls act as triggers to transition between different maps :
+    # - (x, y): Top-left corner coordinates
+    # - Width and height of the trigger area
+    # - InvisibleWall objects handle map transitions
+    mp = [InvisibleWall(630, 0, 120, 2), InvisibleWall(1534, 330, 2, 120),
+          InvisibleWall(630, 862, 120, 2)]
+
+    # main loop
+    running = True
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                exit()
+
+        # clear screen
+        screen.fill((0, 0, 0))
+
+        ground.draw(screen)
+
+        # key press for player
+        key = pygame.key.get_pressed()
+        if key[pygame.K_ESCAPE]:
+            exit()
+
+        player.move(key)
+
+        # Draw the walls
+        for wall in walls:
+            wall.draw(screen)
+
+        # Draw the bots
+        for bot in bots:
+            bot.move()
+            bot.check_collision(walls)
+            bot.check_collision(mp)
+            # if player collide bots it goes to game over screen and pass player position of map01 for new start
+            if player.rect.colliderect(bot.rect):
+                sound_effect = pygame.mixer.Sound('assets/map19/sound/lose.wav')
+                sound_effect.set_volume(0.2)
+                sound_effect.play()
+                pygame.mixer.music.stop()
+                return "game_over", 780, 390
+
+        # player collide check with walls
+        player.check_collision(walls)
+
+        # Options for developer mode:
+        # - Show map number
+        # - Show position of mouse and player on screen
+        # - Show player, map changers, walls and bots rect
+        # - If the space key is pressed, the player teleports to the mouse position
+        if developer_mode:
+            font = pygame.font.SysFont("", 24)
+            x, y = pygame.mouse.get_pos()
+            screen.blit(font.render(f"Developer Mode - map19", True, (255, 0, 0)), (10, 5))
+            screen.blit(font.render(f'X: {x}, Y: {y}', True, (255, 0, 0)), (10, 25))
+            screen.blit(font.render(f'player x : {player.x}', True, (255, 0, 0)), (10, 45))
+            screen.blit(font.render(f'player y : {player.y}', True, (255, 0, 0)), (10, 65))
+
+            if key[pygame.K_SPACE]:
+                player.x = x
+                player.y = y
+
+            player.draw_rect(screen)
+
+            for bot in bots:
+                bot.draw_rect(screen)
+
+            for mp_dev in mp:
+                mp_dev.draw(screen, (0, 0, 255))
+
+            for wall in walls:
+                wall.rect_draw(screen, (0, 255, 0))
+
+        # Frame rate
+        pygame.time.Clock().tick(30)
+
+        # player collide with map changer
+        # return map number and player new position on that map
+        # Due to differentiation in player texture length between map19 and other maps
+        # subtract from player's x position to ensure no collision with the wall occurs.
+        if player.rect.colliderect(mp[0]):
+            return "map15", 721, 640
+
+        # update display
+        pygame.display.flip()
+
+    pygame.quit()
